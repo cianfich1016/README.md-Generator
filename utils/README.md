1ae9fa7ed3c8f9a4370647e67c9ea94a647c2295
Rachel Cianfichi <cianfich@gmail.com>
2021-10-07 16:09:34 -0400

    Framework for switch case statement to create badges.

---
+++ utils/README.md
@@ -0,0 +1,36 @@
+
+
+  # 
+
+  ## Badges
+  ![GNU AGPLv3 License Badge](https://img.shields.io/badge/License-GNUA_GPLv3-blue)
+
+  ## Description
+  
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Tests](#tests)
+  * [Credits](#credits)
+  * [License](#license)
+  * [Contact](#contact)
+
+  ## Installation
+  
+
+  ## Usage
+  
+
+  ## Tests
+  
+
+  ## Credits
+  Contributors to the project were: 
+
+  ## License
+  The license required for this project is GNU AGPLv3.
+
+  ## Contact
+  With any questions or concerns, please contact me via GitHub at []() or by email at .
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-const renderLicenseBadge = (license) => {
+function renderLicenseBadge(license) {
+  switch(license){
+    case "GNU AGPLv3":
+      badge = "![GNU AGPLv3 License Badge](https://img.shields.io/badge/License-GNU_AGPLv3-blue)"
+      break;
+    case "MIT":
+      console.log("no");
+      break;
+    default: 
+      console.log("Sorry.");  
+      break;
+  }
+  return badge;
 }
 
 // TODO: Create a function that returns the license link
@@ -13,7 +25,13 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+
+  # ${data.title}
+
+  ## Badges
+  ${renderLicenseBadge(data.license)}
+
   ## Description
   ${data.description}
 
