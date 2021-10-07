3418d2e87d6b7ce50470890f81d0eda7823b676f
Rachel Cianfichi <cianfich@gmail.com>
2021-10-06 22:05:00 -0400

    Changed writeToFile function to use the imported generateMarkdown function.

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questionsPrompt = () => {
@@ -56,8 +57,8 @@ const questionsPrompt = () => {
 
 // TODO: Create a function to write README file
 const writeToFile = (data) => {
-    fs.writeFile('README.md', JSON.stringify(data), (err) =>
-    err ? console.error(err) : console.log('Success!')
+    fs.writeFile('README.md', generateMarkdown(data), (err) =>
+    err ? console.error(err) : console.log(data)
   );
    
 };
@@ -71,3 +72,4 @@ const init = () => {
 
 // Function call to initialize app
 init();
+
